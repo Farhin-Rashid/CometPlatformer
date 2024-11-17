352546430de4c0589aa570e79002e934e6fb4ad6
Mawlefic <mawlefic@gmail.com>
2024-11-17 16:40:32 -0600

    Added the breakable platforms

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -8,10 +8,10 @@ extends Node2D
 func _ready():
 	player = get_node("Player")
 	respawnLocation = player.global_position
-	print(player.global_position)
-	print(respawnLocation)
+	# print(player.global_position)
+	# print(respawnLocation)
 
 func _process(delta):
 	if player.global_position.y > killZone.global_position.y:
-		print("dead")
+		# print("dead")
 		player.global_position = respawnLocation
