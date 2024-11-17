d55124a771de034547de6f175410508679a05174
Mawlefic <mawlefic@gmail.com>
2024-11-16 21:30:47 -0600

    Adding Fan Mechanic + Adjust Player

---
+++ Scenes/level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var player : CharacterBody2D
+@onready var killZone : Node2D = get_node("Kill Zone")
+@onready var spawnPoint : Node2D = get_node("Respawn")
+@onready var respawnLocation : Vector2
+
+func _ready():
+	player = get_node("Player")
+	respawnLocation = player.global_position
+	print(player.global_position)
+	print(respawnLocation)
+
+func _process(delta):
+	if player.global_position.y > killZone.global_position.y:
+		print("dead")
+		player.global_position = respawnLocation
---
+++ Scripts/fan.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+const UP : int = 0
+const RIGHT : int = 1
+const DOWN : int = 2
+const LEFT : int = 3
+
+@export var fan_force : float = 5000
+@onready var particles : GPUParticles2D = get_node("Particles")
+var zone : Area2D
+var direction : int = 0
+var isActive : bool = true
+# 0 -> UP, 1 -> RIGHT, 2 -> DOWN, 3 -> LEFT
+
+func _ready():
+	zone = %"Air Zone"
+	direction = ceili((rotation_degrees - 5) / 90)
+	var scaleValue = transform.get_scale().x
+	particles.amount *= (scaleValue / 2)
+	particles.process_material.scale_min = scaleValue
+	particles.process_material.scale_max = scaleValue + 1
+
+func _process(delta):
+	if isActive:
+		for body in zone.get_overlapping_bodies():
+			if body.has_method("apply_wind"):
+				body.apply_wind(direction, fan_force)
+
+func toggle():
+	isActive = not isActive
+	particles.emitting = not particles.emitting
+
+func _on_air_zone_body_exited(body):
+	if body.has_method("cancel_wind"):
+		body.cancel_wind(direction)
+
+func _on_timer_timeout():
+	for body in zone.get_overlapping_bodies():
+			if body.has_method("apply_wind"):
+				body.cancel_wind(direction)
+	toggle()
